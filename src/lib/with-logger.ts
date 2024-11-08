d478ebf413ba3e1d2ac6cc0ae8dfc71cd43b0815
git workflows <bdkhulnasoft@gmail.com>
2024-11-08 14:10:46 +0000

    chore(github): add issue form templates

--- src/lib/with-logger.ts
+++ src/lib/with-logger.ts
@@ -1,17 +1,12 @@
-import chalk from 'chalk-template';
 import { Effect, LogLevel, Logger } from 'effect';
 
 export function withLogger(program: Effect.Effect<unknown>) {
   const logger = Logger.make(({ logLevel, message }) => {
-    const args = Array.isArray(message) ? message : [message];
+    const _args = Array.isArray(message) ? message : [message];
     if (logLevel === LogLevel.Info) {
-      globalThis.console.info(...args);
     } else if (logLevel === LogLevel.Debug) {
-      globalThis.console.info(chalk`{magenta ? %s}`, ...args);
     } else if (logLevel === LogLevel.Error) {
-      globalThis.console.error(chalk`{red ! %s}`, ...args);
     } else if (logLevel === LogLevel.Warning) {
-      globalThis.console.warn(chalk`{yellow ! %s}`, ...args);
     } else {
     }
   });
